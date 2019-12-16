bad88203b888f24893f5d85ba660c681230891be
dnarcr00 <47595588+dnarcr00@users.noreply.github.com>
2019-12-16 16:51:16 +0100

    Creacion de los hilos y de sus funciones

--- src/diego.c
+++ src/diego.c
@@ -1,1 +1,52 @@
+//Resumen
+/*
+Dudas:
+*/
+/*
+Trabajando en:
+*/
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/types.h>
+//Tamanio de la cola de solicitudes
+#define tamCola 15
+//Cola de solicitudes
+int cola[tamCola];
+//Vector con el identificador de los "usuarios destacados"
+int i[3]={1,2,3};
+
+/*
+ *	Funciones
+ */
+
+//Funcion del hilo encargado de las invitaciones
+void *despachoInvitador(void *ptr) {
+	printf("Soy el encargado de las invitaciones\n");
+}
+
+//Funcion del hilo encargado de los codigos QR
+void *despachoQR(void *ptr) {
+	printf("Soy el encargado de los codigos QR\n");
+}
+
+//Funcion del AtendedorPRO
+void *despachoAtendedorPRO(void *ptr) {
+	printf("Soy el atendedorPRO\n");
+}
+
+/*
+ *	Main
+ */
+
+int main(){
+printf("tamCola: %d\n",tamCola);
+	//Encargados de las solicitudes de:
+	pthread_t invitador, QR, atendedorPRO;
+	//Creamos los hilos de los "usuarios destacados"
+	pthread_create(&invitador, NULL, despachoInvitador, (void *) &i[0]);
+	pthread_create(&QR, NULL, despachoQR, (void *) &i[1]);
+	pthread_create(&atendedorPRO, NULL, despachoAtendedorPRO, (void *) &i[2]);
+	sleep(1);
+}
+
