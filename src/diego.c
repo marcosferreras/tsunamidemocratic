25c20951d3f9dc00b7e44313eae17c9742744e95
dnarcr00 <47595588+dnarcr00@users.noreply.github.com>
2019-12-22 16:36:24 +0100

    Actualizacion del main

--- src/diego.c
+++ src/diego.c
@@ -11,33 +11,100 @@ Trabajando en:
 #include <pthread.h>
 #include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
+#include <signal.h>
+#include <string.h>
 //Tamanio de la cola de solicitudes
 #define tamCola 15
-//Cola de solicitudes
-int cola[tamCola];
-//Vector con el identificador de los "usuarios destacados"
+#define true 1
+#define false 0
+
+//Estructuras
+
+//Estructura de actividad
+typedef struct actividad{
+	char idUsuario[50];
+	pthread_t hilo;
+} Actividad;
+//Estructura de solicitud
+typedef struct solicitud {
+	char id[50];
+	//0->Sin atender 1->En proceso de atención 2->Atendido 3->Atendido con Antecedentes
+	int atendido;
+	//Tipo 0->Invitación 1->QR
+	int tipo;
+	pthread_t hilo;
+} Solicitud;
+
+
+//Vector con el identificador de los atendedores
 int id[3]={1,2,3};
 //Defino las funciones
-void *despachoInvitador(void *ptr);
-void *despachoQR(void *ptr);
-void *despachoAtendedorPRO(void *ptr);
-
+void *accionesAtendedor(void *ptr);
+void *accionesSolicitud(void *ptr);
+void *accionesCoordinadorSocial(void *ptr);
+void manejadoraSolicitud(int signal);
+void manejadoraFinalizar(int signal);
+void writeLogMessage (char *id , char *msg);
+void inicializarSolicitud(Solicitud* solicitud);
+void inicializarActividad(Actividad* actividad);
+//Cola de solicitudes
+Solicitud colaSolicitudes[tamCola];
+Actividad colaActividadSocial[4];
+//Para la asignacion de ID a las solicitudes
+int contadorSolicitudes;
+//Para saber cuantos hay en cola
+int contadorSolicitudesCola;
+int contadorActividadesCola;
+//Para saber 
+int listaCerrada;
+//Mutex
+pthread_mutex_t mutexLog;
+pthread_mutex_t mutexColaSolicitudes;
+pthread_mutex_t mutexColaSocial;
+//Archivo para los logs
+FILE *logFile;
 /*
  *	Main
  */
 
 int main(){
-printf("tamCola: %d\n",tamCola);
-	//Encargados de las solicitudes de:
+	//Tipo de atendedor 1->Invitacion 2->QR 3->PRO
+	int tipoAtendedor[3]={1,2,3};
+	int i;
+	//Tratamiento de señales
+	struct sigaction sSolicitud={0};
+	struct sigaction sFinalizar={0};
+	sSolicitud.sa_handler=manejadoraSolicitud;
+	sigaction(SIGUSR1,&sSolicitud,NULL);
+	sigaction(SIGUSR2,&sSolicitud,NULL);
+	sFinalizar.sa_handler=manejadoraFinalizar;
+	sigaction(SIGINT,&sFinalizar,NULL);
+	//Inicializar recursos	
+	srand(time(NULL));
+	contadorSolicitudes=0;
+	contadorSolicitudesCola=0;
+	contadorActividadesCola=0;
+	listaCerrada=false;
+	logFile=NULL;
+	for(i=0;i<tamCola;i++){
+		inicializarSolicitud(&colaSolicitudes[i]);
+	}
+	for(i=0;i<4;i++){
+		inicializarActividad(&colaActividadSocial[i]);
+	}
+	//Inicializo los mutex
+	pthread_mutex_init(&mutexColaSocial,NULL);
+	pthread_mutex_init(&mutexLog, NULL);
+	pthread_mutex_init(&mutexColaSolicitudes, NULL);
+	//Encargados de las solicitudes:
 	pthread_t atendedor_1, atendedor_2, atendedor_3;
-	//Enmascaro las señales
-	
-	//Archivo
-	FILE *pa;
-	//Creamos los hilos de los "usuarios destacados"
-	pthread_create(&atendedor_1, NULL, despachoInvitador, (void *) &id[0]);
-	pthread_create(&atendedor_2, NULL, despachoQR, (void *) &id[1]);
-	pthread_create(&atendedor_3, NULL, despachoAtendedorPRO, (void *) &id[2]);
+	//Creamos los hilos de los atendedores
+	pthread_create(&atendedor_1, NULL, accionesAtendedor, (void *) &tipoAtendedor[0]);
+	pthread_create(&atendedor_2, NULL, accionesAtendedor, (void *) &tipoAtendedor[1]);
+	pthread_create(&atendedor_3, NULL, accionesAtendedor, (void *) &tipoAtendedor[2]);
+	//Creamos el hilo del coordinador
+	pthread_create(&coordinador, NULL, accionesCoordinadorSocial, NULL);
 	sleep(1);
 }
 
@@ -84,20 +151,23 @@ void *despachoAtendedorPRO(void *ptr) {
 	num = (0+(rand()%(100)-0)+1);
 	printf("Soy el atendedorPRO\n");
 }
-/*
+
 //Escribimos en el log
-void writeLogMessage (char *id , char *msg, FILE *pa) {
+void writeLogMessage (char *id , char *msg) {
+	pthread_mutex_lock(&mutexLog);
+	if(logFile==NULL){
+		logFile = fopen ("log.txt", "w") ;
+	} else {
+		logFile = fopen ("log.txt", "a") ;
+	}
 	// Calculamos la hora actual
 	time_t now = time (0) ;
 	struct tm *tlocal = localtime (&now) ;
 	char stnow [19];
 	strftime (stnow, 19, " %d/ %m/ %y %H: %M: %S " , tlocal) ;
-
-	// Escribimos en el log
-	logFile = fopen (pa , "a") ;
-	fprintf (pa , "[%s] %s : %s\n" , stnow , id , msg) ;
-	fclose (pa) ;
+	// Escribimos en el log. La primera vez de cada ejecución, borrará el log.txt en caso de que exista.
+	fprintf (logFile , "[%s] %s : %s\n" , stnow , id , msg) ;
+	fclose (logFile);
+	pthread_mutex_unlock(&mutexLog); 
 
 }
-*/
-
