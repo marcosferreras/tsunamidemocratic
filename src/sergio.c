0ac6ef7694108dcfca25c8cd7a5ab01349d6b303
spereb05 <47595378+spereb05@users.noreply.github.com>
2019-12-24 16:11:19 +0100

    actualizacion codigo

--- src/sergio.c
+++ src/sergio.c
@@ -13,7 +13,7 @@
 #define false 0
 
 //Defino las funciones
-void *accionesCoordinadorSocial(void *ptr);
+void *accionesCoordinadorSocial();
 void writeLogMessage (char *id , char *msg);
 
 pthread_mutex_t mutexLog;
@@ -29,13 +29,15 @@ typedef struct actividad{
 
 void inicializarActividad(Actividad* actividad);
 Actividad colaActividadSocial[4];
+void crearHilosActividad();
 
 //Para saber cuantos hay en cola
 int contadorActividadesCola;
 //Para saber 
 int listaCerrada;
 
-pthread_cond_t condicion, usuariosActividad[4];
+pthread_cond_t condicion, usuario[5];
+int idUsuariosActividad[4]= {34,45,12,2};
 
 /*
  *	Main
@@ -55,44 +57,32 @@ int main(){
 	pthread_mutex_init(&mutexLog, NULL);
 	pthread_mutex_init(&mutexColaSolicitudes, NULL);
 	//Encargados de las solicitudes de:
-	pthread_t atendedor_1, atendedor_2, atendedor_3, coordinador;
+	pthread_t coordinador;
 	//Creamos el hilo del coordinador
 	pthread_create(&coordinador, NULL, accionesCoordinadorSocial, NULL);
 
-	// inicializamos las condiciones de espera de los hilos
-	for(i = 0; i < 4; i++){
-		if (pthread_cond_init(&usuariosActividad[i], NULL)!=0) exit(-1);
+	while(true){
+		pause();
 	}
-
-	// inicializamos la condicion de espera del hilo principal
-	if (pthread_cond_init(&condicion, NULL)!=0) exit(-1);
-	
-	listaCerrada = true;
-	pthread_t usuario1, usuario2, usuario3, usuario4;
-
-	pthread_mutex_lock(&mutexColaSocial);
-	pthread_create(&usuario1, NULL, accionesCoordinadorSocial, (void *) &colaActividadSocial[0].idUsuario);
-	pthread_create(&usuario2, NULL, accionesCoordinadorSocial, (void *) &colaActividadSocial[1].idUsuario);
-	pthread_create(&usuario3, NULL, accionesCoordinadorSocial, (void *) &colaActividadSocial[2].idUsuario);
-	pthread_create(&usuario4, NULL, accionesCoordinadorSocial, (void *) &colaActividadSocial[3].idUsuario);
-
-	//pthread_cond_wait(&condicion, &mutexColaSocial);---------------------------------------
-	printf("Va a comenzar la actividad\n");
-	pthread_cond_signal(&usuariosActividad[0]);
-	pthread_mutex_unlock(&mutexColaSocial);
 }
 
-void *accionesCoordinadorSocial(void *ptr){
-	int i;
-	pthread_mutex_lock(&mutexColaSocial);	
+void *accionesCoordinadorSocial(void* ptr){
+	int i, M = 0;
+
+	//pthread_cond_wait(&condicion, &mutexColaSocial);---------------------------------------
+	crearHilosActividad();
+
+	pthread_mutex_lock(&mutexColaSocial);
+	
+	if ((++M)>=4) pthread_cond_signal(&condicion);
 	
 	// Comprobamos si ha finalizado su hilo precedente
-	pthread_cond_wait(&usuariosActividad[*(char *)ptr-1], &mutexColaSocial);
+	pthread_cond_wait(&usuario[*(int *)ptr-1], &mutexColaSocial);
 
-	printf("[UsuarioID: %d -> Ha iniciado la actividad]\n", *(char *)ptr);
+	printf("[UsuarioID: %d -> Ha iniciado la actividad]\n", *(int *)ptr);
 
 	// enviamos señal de finalización del hilo
-	pthread_cond_signal(&usuariosActividad[*(char *)ptr]);
+	pthread_cond_signal(&usuario[*(int *)ptr]);
 
 	pthread_mutex_unlock(&mutexColaSocial);
 }
@@ -124,4 +114,26 @@ void inicializarActividad(Actividad* actividad){
 	pthread_mutex_lock(&mutexColaSocial);
 	strcpy(actividad->idUsuario, "0");
 	pthread_mutex_unlock(&mutexColaSocial);
+}
+
+void crearHilosActividad(){
+	int i;
+
+	// inicializamos las condiciones de espera de los hilos
+	for(i = 0; i < 4; i++){
+		if (pthread_cond_init(&usuario[i], NULL)!=0) exit(-1);
+	}
+	// inicializamos la condicion de espera del hilo principal
+	if (pthread_cond_init(&condicion, NULL)!=0) exit(-1);
+
+	pthread_t usuario1, usuario2, usuario3, usuario4;
+
+	pthread_create(&usuario1, NULL, accionesCoordinadorSocial, (void *) &idUsuariosActividad[0]);
+	pthread_create(&usuario2, NULL, accionesCoordinadorSocial, (void *) &idUsuariosActividad[1]);
+	pthread_create(&usuario3, NULL, accionesCoordinadorSocial, (void *) &idUsuariosActividad[2]);
+	pthread_create(&usuario4, NULL, accionesCoordinadorSocial, (void *) &idUsuariosActividad[3]);
+
+	pthread_cond_wait(&condicion, &mutexColaSocial);
+	printf("Va a comenzar la actividad\n");
+	pthread_cond_signal(&usuario[0]);	
 }
