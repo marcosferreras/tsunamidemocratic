82a2d1210b47c1c5263833030b40aa5102d7c6c2
mario <malvai00@estudiantes.unileon.es>
2019-12-27 13:33:14 +0100

    implementados los logs y los printf

--- src/mario.c
+++ src/mario.c
@@ -18,6 +18,7 @@ typedef struct solicitud {
 	pthread_t hilo;
 } Solicitud;
 Solicitud colaSolicitudes[tamCola];
+FILE *logFile;
 
 pthread_mutex_t mutexLog;
 pthread_mutex_t mutexColaSolicitudes;
@@ -42,62 +43,88 @@ int main(){
 	}
 	pthread_t atendedor_1, atendedor_2, atendedor_3;
 	pthread_create(&atendedor_1, NULL, accionesAtendedor, (void *) &tipoAtendedor[0]);
-	pthread_create(&atendedor_2, NULL, accionesAtendedor, (void *) &tipoAtendedor[1]);
-	pthread_create(&atendedor_3, NULL, accionesAtendedor, (void *) &tipoAtendedor[2]);
-	sleep(10);
+	//pthread_create(&atendedor_2, NULL, accionesAtendedor, (void *) &tipoAtendedor[1]);
+	//pthread_create(&atendedor_3, NULL, accionesAtendedor, (void *) &tipoAtendedor[2])8;
+	pause();
 }
 
 
 void *accionesAtendedor(void *ptrs){
-	int aux, num, contador=0, calculo, espera, apto, posicion=0, cafe=0;	
+	int tipoEvaluacion, id, contador=0, calculo, espera, apto, posicion=0, cafe=0;
+	char atendedor[25], salida[150];
 	//printf("Exito %d", *(int *)ptrs);
+	sprintf(atendedor,"Atendedor_%d",*(int *)ptrs);
 	do{
+		contador=0;//😂️
 		apto=true;
 		cafe++;
 		do{
-			if(contador%2==0){
-				aux=*(int *)ptrs;
-				pthread_mutex_lock(&mutexColaSolicitudes);
-			}		
+			if(contador%2==0 || contador==0){
+					pthread_mutex_lock(&mutexColaSolicitudes);
+			}
+					
 			contador++;
-			num=-1;
+			id=-1;
 			for(int i=0;i<tamCola;i++){
-				if((colaSolicitudes[i].tipo==aux || aux==3) && colaSolicitudes[i].atendido==0){
-					if(num>sacarNumero(colaSolicitudes[i].id) || num==-1){
-						num=sacarNumero(colaSolicitudes[i].id);
-						posicion=i;
+				if((colaSolicitudes[i].tipo==tipoEvaluacion || tipoEvaluacion==3) && colaSolicitudes[i].atendido==0){
+					if((id>sacarNumero(colaSolicitudes[i].id) || id==-1)){
+						if(sacarNumero(colaSolicitudes[i].id)!=0){
+							id=sacarNumero(colaSolicitudes[i].id);
+							posicion=i;
+						}				
 					}
 				}
 			}
-			if(num==-1){
-				aux=3;
+			if(id==-1){
+				tipoEvaluacion=3;
 				if(contador%2==0){
+					//printf("%d-Par: %d\n",*(int *)ptrs,id);
 					pthread_mutex_unlock(&mutexColaSolicitudes);
 					sleep(1);
+				}
 			}
-			}
-		}while(num==-1);
-		colaSolicitudes[posicion].atendido==1;
+		}while(id==-1);
+		colaSolicitudes[posicion].atendido=1;
 		pthread_mutex_unlock(&mutexColaSolicitudes);
+		printf("Atendedor_%d: comienza a procesar la solicitud_%d\n",*(int *)ptrs,id);
+		sprintf(salida,"comienza a procesar la solicitud_%d",id);
+		writeLogMessage(atendedor,salida);
 		srand(time(NULL));
 		calculo=rand()%10+1;
 		if(calculo<=7){
-				espera=rand()%4+1;
+			espera=rand()%4+1;
+			printf("Atendedor_%d: todo correcto al procesar la solicitud_%d\n",*(int *)ptrs,id);
+			sprintf(salida,"todo correcto al procesar la solicitud_%d",id);
+			writeLogMessage(atendedor,salida);
 		}else if(calculo<=9){
 			espera=rand()%5+2;
+			printf("Atendedor_%d: ha ocurrido un error inerperado al procesar los datos de la solicitud_%d\n",*(int *)ptrs,id);
+			sprintf(salida,"ha ocurrido un error inerperado al procesar los datos de la solicitud_%d",id);
+			writeLogMessage(atendedor,salida);
 		}else{
+			printf("Atendedor_%d: el usuario de solicitud_%d posee antecedentes\n",*(int *)ptrs,id);
+			sprintf(salida,"el usuario de solicitud_%d posee antecedentes",id);
+			writeLogMessage(atendedor,salida);
 			espera=rand()%5+6;
 			apto==false;
 		}
 		sleep(espera);
+		printf("Atendedor_%d: el tiempo necesario para atender la solicitud_%d ha sido %d\n",*(int *)ptrs,id,espera);
+		sprintf(salida,"el tiempo necesario para atender la solicitud_%d ha sido %d",id,espera);
+		writeLogMessage(atendedor,salida);
 		pthread_mutex_lock(&mutexColaSolicitudes);
 			if(apto==false){
-				inicializarSolicitud(&colaSolicitudes[posicion]);
+				colaSolicitudes[posicion].atendido=3;
 			} else{
-				colaSolicitudes[posicion].atendido==2;
+				colaSolicitudes[posicion].atendido=2;
 			}
 		pthread_mutex_unlock(&mutexColaSolicitudes);
-		if(cafe%5==0) sleep(10);
+		if(cafe%5==0){
+			printf("Atendedor_%d: procede a tomarse un cafe☕️\n",*(int *)ptrs);
+			sprintf(salida,"procede a tomarse un cafe☕️");
+			writeLogMessage(atendedor,salida);
+			sleep(10);
+		}
 	}while(true);
 }
 
@@ -107,7 +134,7 @@ void *accionesAtendedor(void *ptrs){
 */
 void inicializarSolicitud(Solicitud* solicitud){
 	pthread_mutex_lock(&mutexColaSolicitudes);
-	strcpy(solicitud->id, "0");
+	sprintf(solicitud->id, "0");
 	solicitud->atendido=0;
 	solicitud->tipo=0;
 	solicitud->hilo=0;
@@ -126,6 +153,7 @@ int sacarNumero(char *id){
 }
 
 void writeLogMessage (char *id , char *msg) {
+	int i;
 	pthread_mutex_lock(&mutexLog);
 	if(logFile==NULL){
 		logFile = fopen ("log.txt", "w") ;
@@ -135,8 +163,9 @@ void writeLogMessage (char *id , char *msg) {
 	// Calculamos la hora actual
 	time_t now = time (0) ;
 	struct tm *tlocal = localtime (&now) ;
-	char stnow [19];
-	strftime (stnow, 19, " %d/ %m/ %y %H: %M: %S " , tlocal) ;
+	char stnow [23];
+	//sprintf(stnow,"");
+	strftime (stnow, 23, " %d/ %m/ %y %H: %M: %S " , tlocal) ;
 	// Escribimos en el log. La primera vez de cada ejecución, borrará el log.txt en caso de que exista.
 	fprintf (logFile , "[%s] %s : %s\n" , stnow , id , msg) ;
 	fclose (logFile);
